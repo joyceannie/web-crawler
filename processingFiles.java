d38819415460e3f577786a11c2bdb7213e81c27d
annie <joyceannie111@gmail.com>
2017-06-23 11:24:36 -0700

    Removing noise from the textual content

--- processingFiles.java
+++ processingFiles.java
@@ -1,352 +1,170 @@
-/* PART 1 : WEB CRAWLER 
- * Prerequisite :  
- * Path of specification.csv : /Users/pooja/specification.csv
- * Need to create an empty folder called crawler : /Users/pooja/crawler
- * To run this program, the paths in the code need to be changed accordingly
- * */
-import java.io.IOException;
-import java.io.FileWriter;
-import java.io.FileReader;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.File;
-import java.net.*;
-import java.util.*;
 import java.io.*;
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class Crawler {
+public class processingFiles {
 
-	static int docCount = 0;
-	static int pagesToCrawl = 0;
-	static int maxPagesToCrawl = 0;
-	static BufferedWriter bw = null;
-	static String previousServer = "";
+	/**
+	 *Aho-Corasick algorithm is used for linear time search.
+	 adKeywordFilePath is present in the path "C:/Users/annie/AdBlockerKeywordList.txt".
+	 To run the program, the path needs to be changed properly.
+	 Path of inputDirectory and outputDirectory should be changed properly before running the code.
+	 */
+	private static ConcurrentHashMap<String,Integer> hmap=new ConcurrentHashMap<String,Integer>(); 
+	private static int fileCount;
 	
-	public static void main(String []v)
+
+	private static void removeAd(Document doc1) throws IOException
 	{
-		//HashSet to include unique visited urls
-		Set<String> visitedUrls = new HashSet<>(); 
-		String url="";
-		String domainRestriction = "";
-		try
-		{
-			String line="";
-			BufferedReader fileReader = new BufferedReader(new FileReader("/Users/pooja/specification.csv"));
-			//Read specification.csv 
-			while ((line = fileReader.readLine()) != null) 
-			{   		
-				String[] tokens = line.split(",");
-				
-				//Fetch the seed url
-            	url = tokens[0]; 
-            	//Max pages to crawl
-            	maxPagesToCrawl = Integer.parseInt(tokens[1]); 
-            	
-				if(tokens.length == 3)
-				{
-	            	//Domain restriction if any
-	            	domainRestriction = tokens[2]; 
+		//Removes ad by performing string matching using easylist commonly used by adblockers
+		String adKeywordFilePath="C:/Users/annie/AdBlockerKeywordList.txt";
+		AdUrlChecker adUrlChecker = new AdUrlChecker(adKeywordFilePath);
+		 for (Element element :doc1.select("script")){                
+			 String refLink = element.attr("src");
+			 //String matching is done in linear time using Aho-Corasick algorithm
+		     if(adUrlChecker.isAdUrl(refLink)){
+		    	 element.remove();
+		     }
+		                
+		  }
+	}
+
+	
+	private static void mappingData(Document doc1)
+	{
+		//Maps text from all crawled pages to a concurrent hashmap
+			for(Element div:doc1.select("div"))
+			{
+				if(hmap.containsKey(div.text())){
+				hmap.put(div.text(), hmap.get(div.text())+1);	
 				}
-				else
-				{
-					domainRestriction = "NO RESTRICTION";
+				else{
+				hmap.put(div.text(), 1);
 				}
+			}	
 			
-                pagesToCrawl = maxPagesToCrawl;
-                
-                //Create report.html
-                bw = new BufferedWriter(new FileWriter("/Users/pooja/crawler/report.html"));
-                
-                //Keeping track of previous web server visited to maintain politeness policy
-                previousServer = new URL(url).getHost();
-			}
-			fileReader.close();
-        } 
-        catch (Exception e)
-		{
-            e.printStackTrace();
-        } 
-		
-		//Process the seed url
-		processSeed(url, pagesToCrawl, domainRestriction, visitedUrls);	
-		
-	}
-	
-	/*
-	 *	Method : robotSafe 
-	 *	Input parameter : URL url
-	 *	Return value : boolean
-	 *	Given an URL, this method checks the robots.txt and returns appropriate boolean value true/false if the page can/cannot be crawled 
-	 * 
-	 */
-	public static boolean robotSafe(URL url)
-	{
-	    String DISALLOW = "Disallow:";
-	    String strHost = url.getHost();
-
-		//Form URL of the robots.txt file
-	    String strRobot = "http://" + strHost + "/robots.txt";
-	    URL urlRobot;
-	    try
-	    {
-	    	urlRobot = new URL(strRobot);
-		}
-	    catch (MalformedURLException e)
-	    {  
-		    return false;
-		}
-
-	    String strCommands;
-	    try
-	    {
-	       InputStream urlRobotStream = urlRobot.openStream();
-
-		   //Read in entire file
-	       byte b[] = new byte[1000];
-	       int numRead = urlRobotStream.read(b);
-	       strCommands = new String(b, 0, numRead);
-	       while (numRead != -1)
-	       {
-	          numRead = urlRobotStream.read(b);
-	          if (numRead != -1)
-	          {
-	             String newCommands = new String(b, 0, numRead);
-		         strCommands += newCommands;
-	          }
-		    }
-	       urlRobotStream.close();
-		}
-	    catch (IOException e)
-	    {
-		    //If there is no robots.txt file, it is OK to crawl
-		    return true;
-		}
-	    catch (StringIndexOutOfBoundsException stre)
-	    {
-		    //If there is no robots.txt file, it is OK to crawl
-		    return true;
-		}
-	    catch(IllegalArgumentException a)
-	    {
-	    	return true;
-	    }
-
-	    //If there are no "disallow" values, then they are not blocking anything
-	    if (strCommands.contains(DISALLOW)) 
-	    {
-	        String[] split = strCommands.split("\n");
-	        String mostRecentUserAgent = null;
-	        for (int i = 0; i < split.length; i++) 
-	        {
-	            String line = split[i].trim();
-	            
-	            //Check for the user-agent
-	            if (line.toLowerCase().startsWith("user-agent")) 
-	            {
-	                int start = line.indexOf(":") + 1;
-	                int end   = line.length();
-	                mostRecentUserAgent = line.substring(start, end).trim();
-	            }
-	            else if (line.startsWith(DISALLOW))
-	            {
-	                    int start = line.indexOf(":") + 1;
-	                    int end   = line.length();
-	                    String rule = line.substring(start, end).trim();
-	                    if(mostRecentUserAgent.equals("*"))
-	                    {
-	                    	String path = url.getPath();
-	                    	
-	                    	//Allows everything if BLANK
-	        	            if (rule.length() == 0) return true;
-	        	            
-	        	            //Allows nothing if /
-	        	            if (rule == "/") return false;      
-
-	        	            //Check the path in the rule 
-	        	            if (rule.length() <= path.length())
-	        	            { 
-	        	                String pathCompare = path.substring(0, rule.length());
-	        	                if (pathCompare.equals(rule)) return false;
-	        	            }    	       
-	                    }
-	             }
-	           }
-	        }    
-		return true;
-	}
-	
-	/*
-	 *	Method : processSeed 
-	 *	Input parameter : String URL, int remainingPages, String domainRestriction,Set<String> visited
-	 *	Return value : void
-	 *	Crawl the URL if it is not crawled within the domain restriction
-	 * 
-	 */
-	
-	public static void processSeed(String URL, int remainingPages, String domainRestriction,Set<String> visited)
-	{
-		FileWriter fWriter = null;
-		BufferedWriter writer = null;
-		String localPath = "";
-		int httpStatus;	
-		
-		String reportData = "<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}</style><title>Web Crawler Statistics</title></head><body><h1>Web Crawler Statistics</h1><table style=\"width:100%\"><th>Live URL</th><th>Repository link</th><th>HTTP Status Code</th><th>No of outlinks</th><th>No of images</th>";
-		
-		
-		//Do not process the URL if it is empty or has been already visited
-		if(URL.isEmpty() || visited.contains(URL))
-			return;
-		
-		try
-		{
-			//Check if there are still pages to be crawled
-			if(pagesToCrawl >=1)
-			{
-				//Check if the page can be crawled
-				if(robotSafe(new URL(URL)))
-				{
-					//Add the url to the visited set
-					visited.add(URL);
-					pagesToCrawl--;		
-		
-					try
-					{		
-						//Retrieve the page content, status code
-						Connection.Response response = Jsoup.connect(URL).execute();	
-						Document doc = response.parse();
-						httpStatus = response.statusCode();
-						
-						System.out.println("URL :" + URL + " STATUS : "+httpStatus);		
-						
-						//Download the pages into crawler folder
-						try 
-						{
-							File f = new File("/Users/pooja/crawler");
-							if(!f.exists())
-								f.mkdirs();
-							String filename = "/Users/pooja/crawler/doc" + ++docCount + ".html";
-							localPath = "file://"+ filename;
-						    fWriter = new FileWriter(filename);
-						    writer = new BufferedWriter(fWriter);
-						    writer.write(doc.html()); 
-						    writer.close();
-						}
-						catch (Exception e)
-						{
-							System.out.println("Error in writing to html file "+e);
-						}
-						
-						//Calculate number of images in the page
-						Elements images = doc.getElementsByTag("img");
-						int noOfImages = images.size();
-						
-						//Calculate the number of outlinks in the page
-						Elements questions = doc.select("a[href]");
-						int noOfOutlinks = questions.size();
-						
-						for(Element link: questions)
-						{					
-							String checkLink = link.attr("abs:href");	
-							//Handling # in <a href> to prevent pointing to the same page and decrementing the outlink count
-							if(checkLink.indexOf("#") != -1)
-							{
-								noOfOutlinks--;
-							}		
-						}	
-						
-						//report.html
-						try
-						{
-							//For the first page crawled, write the initial content of report.html
-							if(pagesToCrawl == maxPagesToCrawl - 1)
-							{
-								bw.write(reportData);		
-							}
-							
-							//Add the page statistics -> live URL, local repository path, status, number of images and outlinks
-							bw.write("<tr><td><a href=\""+URL+"\">"+ URL +"</a></td><td><a href=\""+localPath+"\">"+localPath+"</a></td><td>"+httpStatus+"</td><td>"+noOfOutlinks+"</td><td>"+noOfImages+"</td></tr>");
-							
-							//Close the report.html after the last page is crawled
-							if(pagesToCrawl == 0)
-							{
-								bw.write("</table></body></html>");
-								bw.close();
-							}
-						}
-						catch(Exception e1)
-						{
-							System.out.println("Exception e1 : " +e1);	
-						}
-						
-						for(Element link: questions)
-						{	
-							String outlink = link.attr("abs:href");
-							java.net.URL outlinkURL = new URL(outlink);
-							
-							//Handling # in <a href> to prevent pointing to the same page 
-							if(outlink.indexOf("#") != -1)
-							{
-								//System.out.println(outlink);
-								String[] getActualUrl = outlink.split("#"); 
-								outlink = getActualUrl[0];
-							}
-							
-							if(domainRestriction.equals("NO RESTRICTION"))
-							{
-								processSeed(outlink, pagesToCrawl, domainRestriction, visited);
-							}
-							else
-							{
-								//Check the domain restriction
-								if(domainRestriction.indexOf(outlinkURL.getHost()) != -1)
-								{
-									//If the web server was the immediate previous one visited, then crawl the page after a second delay
-									if(previousServer.equals(new URL(URL).getHost()))
-									{
-										try
-										{
-											//Politeness policy - 1second delay
-											Thread.sleep(1000);
-										}
-										catch(InterruptedException t)
-										{}
-									}
-									else
-									{
-										previousServer = new URL(URL).getHost();
-									}
-									
-									processSeed(outlink, pagesToCrawl, domainRestriction, visited);
-								}
-							}	
-						}			
-					}
-					catch(MalformedURLException ex)
-					{
-						//do nothing -> invalid starting URL
-					}
-					catch(IOException ex)
-					{
-						//System.out.println("Error in http request"+ex+" url:" + URL);
-					}
+			for (Element link:doc1.select("a[href]")){
+				if(hmap.containsKey(link.text())){
+				hmap.put(link.text(), hmap.get(link.text())+1);	
 				}
-		}
-			else
-			{
-				//url cannot be crawled as it is disallowed by robots.txt
-				//System.out.println("[NOT SAFE ROBOTS.TXT]" + URL);
+				else{
+				hmap.put(link.text(), 1);
+				}	
+			}
+			
+			for(Element ulTag:doc1.select("ul")){
+				if(hmap.containsKey(ulTag.text())){
+					hmap.put(ulTag.text(), hmap.get(ulTag.text())+1);	
+				}
+				else{
+					hmap.put(ulTag.text(), 1);
+				}
+		}	
+			
+			
+	}
+	
+	private static void updateHashmap(){
+		//Removing text from hashmap if frequency is below a threshold
+		for (String key:hmap.keySet()){
+			if(hmap.containsKey(key) && hmap.get(key)<=fileCount/2){
+				hmap.remove(key);
+	
 			}
 		}
-		catch(MalformedURLException malEx)
+	
+	}
+	
+	
+	
+	private static void removeNoise(Document doc1){
+		//If the text in a particular tag is present in the hashmap, it is recognized as noise.
+		for(Element div:doc1.select("div"))
 		{
-			//do nothing -> invalid starting URL
+			if(hmap.containsKey(div.text())){
+				div.remove();
+			}
 		}
+		
+		for(Element ulTag:doc1.select("ul")){
+			if(hmap.containsKey(ulTag.text())){
+				ulTag.remove();
+			}
+		}
+
+		for(Element link:doc1.select("a"))
+		{
+			if(hmap.containsKey(link.text())){
+				link.remove();
+			}
+		}
+		
+	}
+	
+
+	private static void createFile(Document doc1,int docCount){
+		//Updated document is written to the output directory
+		FileWriter fWriter = null;
+		BufferedWriter writer =null;
+		String outputDirectory="H:/SCU/Courses/web search/webCrawler/finalData/";
+		File f = new File(outputDirectory);
+		if(!f.exists())
+			f.mkdirs();
+		String filename = outputDirectory+"doc" + docCount + ".html";
+		
+		
+	    try {
+	    	fWriter = new FileWriter(filename);
+	    	writer = new BufferedWriter(fWriter);
+			writer.write(doc1.html());
+			writer.close();
+	    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+		
+	
+	public static void main(String[] args) throws IOException {
+
+		 String inputDirectory="H:/SCU/Courses/web search/webCrawler/output/";
+		 fileCount=new File(inputDirectory).list().length;
+		//fileCount=3;
+		 Document doc1;
+	
+		 //Reading all input files and mapping the text
+		for (int i=1;i<=fileCount;++i){
+			doc1 = Jsoup.parse(new File(inputDirectory+"doc"+i+".html"),"ISO-8859-1");
+			mappingData(doc1);					
+	
+		}
+		
+		//Removing less frequent text from hashmap as it is main content
+		updateHashmap();
+
+		//Reading all the input files
+		for (int i=1;i<=fileCount;i++){
+			doc1 = Jsoup.parse(new File(inputDirectory+"doc"+i+".html"),"ISO-8859-1");
+			//Hashmap contains noise part which is removed from the files
+			removeNoise(doc1);
+			
+			//Removing all nav tags
+			for(Element element : doc1.select("nav")){
+				element.remove();		 
+			}
+			
+			//Ads are removed
+			removeAd(doc1);
+			
+			//Writing the modified document to disc
+			createFile(doc1,i);	
+
+		}
+		
 	}
 
 }
+
